8db050fbacfef60ae45c9eb3c2586bf6f05fbb31
Gregory <gregoryt@praxis.co.za>
2022-01-13 20:21:51 +0200

    Revert "Export button"
    This reverts commit 417dc3930a26b9bfc6bacd60cf0b07cacb0b01de.

--- src/app/views/dashboard/home/application-charts/application-charts.component.ts
+++ src/app/views/dashboard/home/application-charts/application-charts.component.ts
@@ -776,28 +776,5 @@ export class ApplicationChartsComponent implements OnInit, OnChanges {
       this.facultyDashboardCharts.push(barGraph);
     }
   }
-  public exportRepot()  
-  {  
-    var data = document.getElementById('report');
-     
-    if(data !== null){
-      html2canvas(data).then(canvas => {  
-        // Few necessary setting options  
-        var imgWidth = 208;   
-        var pageHeight = 295;    
-        var imgHeight = canvas.height * imgWidth / canvas.width;  
-        var heightLeft = imgHeight;  
-    
-        const contentDataURL = canvas.toDataURL('image/png')  
-        let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
-        var width = pdf.internal.pageSize.getWidth();
-        var height = pdf.internal.pageSize.getHeight();
-        var position = 0;  
-        pdf.addImage(contentDataURL, 'PNG', 0, position, width, height)  
-        pdf.save('Application_Report.pdf'); // Generated PDF   
-      });
-      
-    }
-    
-  }
+  
 }
